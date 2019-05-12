6d15e5e2fa52d56c45768347e752f516e2033f5b
xiaoFeng <804875728@qq.com>
2019-05-12 14:07:48 +0800

    歌词效果实现

--- Js/main.js
+++ Js/main.js
@@ -217,3 +217,24 @@ var Fm={
   
 
 }
+
+$.fn.boomText=function(type){
+  type=type|| 'rollIn'
+  this.html(function(){
+    var arr=$(this).text()
+    .split('').map(function(word){
+      return '<span class="boomText">'+word+'</span>'
+    })
+    return arr.join('')
+  })
+
+  var index=0
+  var $boomTexts=$(this).find('span')
+  var clock=setInterval(function(){
+    $boomTexts.eq(index).addClass('animated'+type)
+    index++
+    if(index>=$boomTexts.length){
+      clearInterval(clock)
+    }
+  },300)
+}
